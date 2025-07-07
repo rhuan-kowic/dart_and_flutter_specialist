511603b765cd835f7e9d399acc641cfff9408d6d
Rhuan Kowic Santos <dev.rhuankowic@gmail.com>
2025-07-07 18:50:49 -0300

    init: Iniciando projeto agenda de compromissos.

---
+++ dart_projects/agenda_de_compromissos/bin/agenda_de_compromissos.dart
@@ -0,0 +1,5 @@
+import 'package:agenda_de_compromissos/agenda_de_compromissos.dart' as agenda_de_compromissos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${agenda_de_compromissos.calculate()}!');
+}
---
+++ dart_projects/agenda_de_compromissos/test/agenda_de_compromissos_test.dart
@@ -0,0 +1,8 @@
+import 'package:agenda_de_compromissos/agenda_de_compromissos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
