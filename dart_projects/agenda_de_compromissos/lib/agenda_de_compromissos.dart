99c2dba7f87c922909e6f63ac7e7f1d525d85e8f
Rhuan Kowic Santos <dev.rhuankowic@gmail.com>
2025-07-09 16:49:15 -0300

    feat: adicionando funcao de listar compromissos agendados.

--- dart_projects/agenda_de_compromissos/lib/agenda_de_compromissos.dart
+++ dart_projects/agenda_de_compromissos/lib/agenda_de_compromissos.dart
@@ -1,3 +1,23 @@
-int agendaDeCompromissos() {
-  return 6 * 7;
+import 'package:agenda_de_compromissos/models/compromisso_agendado.dart';
+
+void agendaDeCompromissos() {
+  print("Bem-vindo ao agenda de compromissos");
+  List<CompromissoAgendado> listaDeCompromisso = [
+    CompromissoAgendado(
+      nome: "Estudar Dart",
+      dataHorario: DateTime(2025, 7, 10, 6, 30),
+      descricao: "Ver no Notion, qual meterial estudar",
+    ),
+  ];
+  listarCompromissoAgendadados(listaDeCompromisso);
+}
+
+void listarCompromissoAgendadados(
+  List<CompromissoAgendado> listaDeCompromisso,
+) {
+  int contador = 0;
+  for (var compromisso in listaDeCompromisso) {
+    print("indice: $contador, ${compromisso.toString()}");
+    contador++;
+  }
 }
