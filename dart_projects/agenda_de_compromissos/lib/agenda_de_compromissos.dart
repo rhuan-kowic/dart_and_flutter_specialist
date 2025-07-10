f7aa0c0f0f7f9789635fea913b7455de0757dbf9
Rhuan Kowic Santos <dev.rhuankowic@gmail.com>
2025-07-09 21:28:46 -0300

    feat: Adicionando funcao adicionarCompromissoAgendado

--- dart_projects/agenda_de_compromissos/lib/agenda_de_compromissos.dart
+++ dart_projects/agenda_de_compromissos/lib/agenda_de_compromissos.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:agenda_de_compromissos/models/compromisso_agendado.dart';
+import 'package:intl/intl.dart';
 
 void agendaDeCompromissos() {
   print("Bem-vindo ao agenda de compromissos");
@@ -9,10 +12,11 @@ void agendaDeCompromissos() {
       descricao: "Ver no Notion, qual meterial estudar",
     ),
   ];
-  listarCompromissoAgendadados(listaDeCompromisso);
+  adicionarCompromissoAgendado(listaDeCompromisso);
+  listarCompromissoAgendadado(listaDeCompromisso);
 }
 
-void listarCompromissoAgendadados(
+void listarCompromissoAgendadado(
   List<CompromissoAgendado> listaDeCompromisso,
 ) {
   int contador = 0;
@@ -20,4 +24,45 @@ void listarCompromissoAgendadados(
     print("indice: $contador, ${compromisso.toString()}");
     contador++;
   }
+}
+
+void adicionarCompromissoAgendado(
+  List<CompromissoAgendado> listaDeCompromisso,
+) {
+  print("Digite o nome do compromisso: ");
+  String? nomeDoCompromisso = stdin.readLineSync();
+
+  if (nomeDoCompromisso == null) {
+    print("Nome do compromisso invalido.");
+    return;
+  }
+
+  print(
+    "Digite a data e horario do compromisso. Utilize este padrao: (09/07/2025 21:45): ",
+  );
+  DateTime? dataFormatada;
+  String? dataHoraString = stdin.readLineSync();
+
+  if (dataHoraString == null) {
+    print("Data e horario do compromisso invalido.");
+    return;
+  }
+  try {
+    DateFormat formato = DateFormat("dd/MM/yyyy HH:mm");
+    dataFormatada = formato.parseStrict(dataHoraString);
+  } catch (e) {
+    print("Error: $e");
+  }
+
+  print("Digite a descricao do compromisso(opcional)");
+  String? descricao = stdin.readLineSync();
+
+
+  CompromissoAgendado compromisso = CompromissoAgendado(
+    nome: nomeDoCompromisso,
+    dataHorario: dataFormatada,
+    descricao: descricao?.isEmpty == true ? "Sem descricao" : descricao,
+  );
+
+  listaDeCompromisso.add(compromisso);
 }
